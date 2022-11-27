e9e5c8545a0810dc2a05545b567e84f6bdd20579
TeamHackersOfficial <117904240+TeamHackersOfficial@users.noreply.github.com>
2022-11-27 19:12:23 +0530

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -332,7 +332,7 @@ bot
 
 bot.command("channels", (ctx) => {
   ctx.reply(
-    "_✅ Access Premium Channels_\n\n*All Indian Movies\n@WatchIndianMovies\n18+ Content\nGirls Don't Join\n@EighteenPlusVideosBot*",
+    "_✅ Access Premium Channels_\n\n*All Indian Movies\n@WatchIndianMovies\n🔞 Content 💦\n@EighteenPlusVideosBot*",
     { parse_mode: "Markdown" },
   );
 });
